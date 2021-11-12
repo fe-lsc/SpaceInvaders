89de4242916a61c9112561953a7ec3a707e39d18
PPSasso <pp_sasso@hotmail.com>
2021-11-11 22:15:12 -0200

    Game beginning

---
+++ Assets/main.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class main : MonoBehaviour
+{
+
+    public Material mat;    //Material utilizado para renderizar os objetos do jogo
+    public Vector2 sb;      //Aparentemente a tela, mas ainda não tenho certeza kkk
+
+    bool playState = false; //Booleana responsável por renderizar ou não os objetos do jogo
+    public float posY;      //Posição dos objetos no eixo Y
+    public float posX;      //Posição dos objetos no eixo X
+    public float dx;        //Velocidade no eixo X
+    public float dy;        //Velocidade no eixo Y
+    
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
